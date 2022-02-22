be67ff9ed58d2a1c256e788e637a291568913349
AC59852 <austin.caron1@gmail.com>
2022-02-22 02:22:31 -0500

    Separated controllers. Added commented code, with summaries and examples. Fixed the lists in J1 to have the right calorie counts. Cleaned variable declaration to reduce redundancy.

---
+++ Controllers/DiceController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Loops_W2022.Controllers
+{
+    public class DiceController : ApiController
+    {
+        [HttpGet]
+        [Route("api/J2/DiceGame/{m}/{n}")]
+        public string DiceGame(int m, int n)
+        /// <summary>
+        /// This controller takes in 2 parameters, and looks to find how many
+        /// times those 2 numbers can make the sum of 10.
+        /// 
+        /// If there is only one way, the message content will differ from if there are multiple ways,
+        /// but the output number will always be the TOTAL amount of times to make 10
+        /// 
+        /// <example>
+        /// Using 6 as the first die number, and 8 as the second
+        /// <param name="number1">6</param>
+        /// <param name="number2">8</param>
+        /// 
+        /// <return>There are 5 total ways to get the sum of 10</return>
+        /// </example>
+        /// </summary>
+
+        {
+            // dice start on the number 1
+            int start = 1;
+
+            // numbers and numbers2 are lists that hold each number before and including the user inputted number in the link
+            List<int> numbers = new List<int>();
+            List<int> numbers2 = new List<int>();
+
+            // int for holding the total results for sum of 10
+            int total = 0;
+
+            // output declaration
+            string message;
+
+            // loop through the provided numbers and add them to separate lists
+            for (int counter = start; counter <= m; counter++)
+            {
+                numbers.Add(counter);
+            }
+
+            for (int counter = start; counter <= n; counter++)
+            {
+                numbers2.Add(counter);
+            }
+
+            /// wrapping a foreach loop in another foreach loop
+            /// this allows usage of the number integer inside of the second loop
+            /// which then checks if the two numbers can be added to equal 10, if so, increment the counter by 1
+            foreach (int number in numbers)
+            {
+                foreach (int number2 in numbers2)
+                {
+                    if (number + number2 == 10) total++;
+                }
+            }
+
+            // if the total amount of combination of numbers equaling 10 only equals 1, change the output message slightly to reflect that.
+            if (total == 1) message = "there is " + total + " way to get the sum of 10.";
+            else message = "there are " + total + " total ways to get the sum of 10.";
+
+            return message;
+        }
+    }
+}
---
+++ Controllers/MenuController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Loops_W2022.Controllers
+{
+    public class MenuController : ApiController
+    {
+        [HttpGet]
+        [Route("api/J1/Menu/{burger}/{drink}/{side}/{dessert}")]
+        public string Menu(int burger, int drink, int side, int dessert)
+        /// <summary>
+        /// This controller takes in 4 parameters from 1 to 4, and calculates the amount of calories
+        /// the 4 combined items have in total
+        /// 
+        /// Data for each list is stored in its own array, separating the drinks from the sides, etc
+        /// 
+        /// <example>
+        /// Combining the list items:
+        /// <param name="item1">2 (431)</param>
+        /// <param name="item2">1 (130)</param>
+        /// <param name="item3">3 (70)</param>
+        /// <param name="item4">4 (0)</param>
+        /// 
+        /// <return>Your total calorie count is: 661</return>
+        /// </example>
+        /// </summary>
+        {
+            // the base output that will be returned, not including the totalCalories result
+            string message = "Your total calorie count is: ";
+
+            // arrays containing data for calories of each item on the menu
+            int[] burgers = { 461, 431, 420, 0 };
+            int[] drinks = { 130, 160, 118, 0 };
+            int[] sides = { 100, 57, 70, 0 };
+            int[] desserts = { 167, 266, 75, 0 };
+
+            // defining a starting integer to combine everything in to.
+            int totalCalories;
+
+            /// the totalCalories is taking the number input by the user, and reducing it by one
+            /// this is because the starting position in an array is 0
+            /// the variable inside of the bracket is the index of the item in the array
+            totalCalories = burgers[burger - 1] + drinks[drink - 1] + sides[side - 1] + desserts[dessert - 1];
+
+            // finalized result
+            message = message + totalCalories;
+
+            return message;
+        }
+    }
+}
